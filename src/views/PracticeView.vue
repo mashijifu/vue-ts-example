2f4afa8fa5e89a24e8f213768d4947e136bcdc46
mashijifu <4ts0t0i7c@gmail.com>
2024-04-23 22:14:13 +0900

    v-bindディレクティブを使用してclassの設定

--- src/views/PracticeView.vue
+++ src/views/PracticeView.vue
@@ -1,27 +1,24 @@
 <template>
   <div>
-    <input v-model="price" />
-    <!-- 入力された金額に対して10%を付与した値を表示 -->
-    <p>{{ taxPrice }}</p>
+    <!-- 以下isRedがtrueであれば赤文字で表示させる -->
+    <p :class="{ red: isRed }">見出し1</p>
   </div>
 </template>
 <script lang="ts">
-import { defineComponent, ref, watch } from "@vue/composition-api";
+import { defineComponent } from "@vue/composition-api";
 
 export default defineComponent({
   setup() {
-    // ウォッチャーを使って、priceの数値に10%を掛けた値を表示する処理を記述
-    const price = ref(0);
-    const taxPrice = ref(0);
-
-    watch(price, () => {
-      taxPrice.value = Math.floor(price.value * 1.1);
-    });
+    const isRed = true;
 
     return {
-      price,
-      taxPrice,
+      isRed,
     };
   },
 });
 </script>
+<style scoped>
+.red {
+  color: red;
+}
+</style>
