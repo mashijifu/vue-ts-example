d6938956097a438b9ce0772df31b794f3c2f1c6e
mashijifu <4ts0t0i7c@gmail.com>
2024-04-26 18:10:22 +0900

    v-forディレクティブの課題２

--- src/views/PracticeView.vue
+++ src/views/PracticeView.vue
@@ -1,9 +1,16 @@
 <template>
   <div>
-    <!-- v-forを使ってdataListの配列をリスト表示しなさい -->
-    <ul>
-      <li v-for="(data, index) in dataList" :key="index">{{ data }}</li>
-    </ul>
+    <!-- v-forを使ってdataListの配列オブジェクトをテーブルの要素として表示しなさい -->
+    <table>
+      <tr>
+        <th>id</th>
+        <th>name</th>
+      </tr>
+      <tr v-for="(item, index) in dataList" :key="index">
+        <td>{{ item.id }}</td>
+        <td>{{ item.name }}</td>
+      </tr>
+    </table>
   </div>
 </template>
 <script lang="ts">
@@ -11,7 +18,11 @@ import { defineComponent } from "@vue/composition-api";
 
 export default defineComponent({
   setup() {
-    const dataList = ["バナナ", "イチゴ", "スイカ"];
+    const dataList = [
+      { id: 1, name: "バナナ" },
+      { id: 2, name: "イチゴ" },
+      { id: 3, name: "スイカ" },
+    ];
 
     return {
       dataList,
