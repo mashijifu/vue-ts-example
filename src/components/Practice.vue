167e9ec0456d5148045e7b4f8694b9f8f18b43eb
mashijifu <4ts0t0i7c@gmail.com>
2024-05-23 23:23:38 +0900

    propsの課題1

---
+++ src/components/Practice.vue
@@ -0,0 +1,24 @@
+<script lang="ts">
+import { defineComponent } from "@vue/composition-api";
+
+export default defineComponent({
+  name: "PracticeComponent",
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+  },
+});
+</script>
+
+<template>
+  <div>
+    <p>{{ title }}</p>
+    <p>{{ description }}</p>
+  </div>
+</template>
--- src/views/PracticeView.vue
+++ src/views/PracticeView.vue
@@ -1,74 +1,17 @@
-<template>
-  <div>
-    <select v-model="selectedLangage">
-      <option disabled value="">選択してください</option>
-      <option
-        v-for="(language, index) in languages"
-        :value="language.value"
-        :key="index"
-      >
-        {{ language.label }}
-      </option>
-    </select>
-    <select v-if="selectedLangage !== ''" v-model="selectedFramework">
-      <option disabled value="">選択してください</option>
-      <option
-        v-for="(framework, index) in frameworks[selectedLangage]"
-        :value="framework.value"
-        :key="index"
-      >
-        {{ framework.label }}
-      </option>
-    </select>
-  </div>
-</template>
 <script lang="ts">
-import { ref, defineComponent } from "@vue/composition-api";
+import { defineComponent } from "@vue/composition-api";
+import Practice from "@/components/Practice.vue";
 
 export default defineComponent({
-  setup() {
-    const languages = [
-      {
-        value: "php",
-        label: "PHP",
-      },
-      {
-        value: "java",
-        label: "Java",
-      },
-    ];
-
-    const frameworks = {
-      php: [
-        {
-          value: "laravel",
-          label: "Laravel",
-        },
-        {
-          value: "cakephp",
-          label: "CakePHP",
-        },
-      ],
-      java: [
-        {
-          value: "spring",
-          label: "Spring",
-        },
-        {
-          value: "play",
-          label: "Play",
-        },
-      ],
-    };
-    const selectedLangage = ref("");
-    const selectedFramework = ref("");
-
-    return {
-      selectedLangage,
-      selectedFramework,
-      languages,
-      frameworks,
-    };
+  name: "PracticeView",
+  components: {
+    Practice,
   },
 });
 </script>
+
+<template>
+  <div>
+    <Practice title="タイトル" description="説明です。" />
+  </div>
+</template>
