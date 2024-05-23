e012a770fcbd2dac6c2be25f68ab14ada5da3cd1
mashijifu <4ts0t0i7c@gmail.com>
2024-05-23 23:33:10 +0900

    props課題2

--- src/views/PracticeView.vue
+++ src/views/PracticeView.vue
@@ -1,4 +1,4 @@
-<script lang="ts">
+<!-- <script lang="ts">
 import { defineComponent } from "@vue/composition-api";
 import Practice from "@/components/Practice.vue";
 
@@ -13,5 +13,41 @@ export default defineComponent({
 <template>
   <div>
     <Practice title="タイトル" description="説明です。" />
+  </div>
+</template> -->
+
+<script lang="ts">
+import { defineComponent } from "@vue/composition-api";
+import Practice from "@/components/Practice.vue";
+
+export default defineComponent({
+  name: "PracticeView",
+  components: {
+    Practice,
+  },
+  setup() {
+    const items = [
+      {
+        title: "Item 1",
+        description: "Description 1",
+      },
+      {
+        title: "Item 2",
+        description: "Description 2",
+      },
+    ];
+
+    return { items };
+  },
+});
+</script>
+
+<template>
+  <div>
+    <ul>
+      <li v-for="(item, index) in items" :key="index">
+        <Practice :title="item.title" :description="item.description" />
+      </li>
+    </ul>
   </div>
 </template>
